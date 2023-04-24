98383897d80582af7e97878661780c0626d2ca49
oopkale <you@example.com>
2023-04-24 13:51:27 -0700

    04/24 aoooo

--- app/routes/evnt.py
+++ app/routes/evnt.py
@@ -2,9 +2,9 @@ from app import app
 import mongoengine.errors
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user
-from app.classes.data import Event
+from app.classes.data import Event, eventcomment
 from app.classes.forms import EventForm, eventcommentForm
-from app.classes.forms import eventcommentForm
+
 from flask_login import login_required
 import datetime as dt
 
