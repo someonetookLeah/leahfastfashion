6e0b57f9848422ffbd1974022cbd68e25a2f195f
natmeh <nataliemehs@gmail.com>
2023-04-28 09:57:35 -0700

    nnn

--- main.py
+++ main.py
@@ -8,3 +8,28 @@ if __name__ == "__main__":
     
     # app.run(debug="True", ssl_context='adhoc')
     app.run(debug="True",use_reloader=True, ssl_context=('cert.pem', 'key.pem'))
+
+
+    
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/static/')
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('feedback.html')
+
+@socketio.on('client_message')
+def receive_message (client_msg):
+    emit('server_message', client_msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app)
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/static/')
+socketio = SocketIO(app)
